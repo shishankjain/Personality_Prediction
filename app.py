805cf982c672839160466c76e4565ef457212cf5
Shishank Jain <50173927+shishankjain@users.noreply.github.com>
2021-04-27 02:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pickle
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 import re
+from pyresparser import ResumeParser
 import numpy as np
-from ipywidgets import widgets, interact
+
 from flask_sqlalchemy import SQLAlchemy
 import os
 from werkzeug.utils import secure_filename
@@ -13,21 +14,37 @@ from werkzeug.datastructures import  FileStorage
 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config ['UPLOAD_FOLDER'] = "C:\\Users\\jshas\\Desktop\\Personality_Prediction\\Personality_Prediction\\static"
+app.config ['UPLOAD_FOLDER'] = "G:\\NEWW\\Personality_Prediction"
 db = SQLAlchemy(app)
 
+
+#SKILLS REQUIRED in SET
+text={'Python','Machine Learning', 'Java', 'SQL', 'C'}
+#EXPERIENCE RANGE MENTIONED IN LIST
+req_experience=[0,2]
+
+
+#CREATING DATABASE table name=database
 class database(db.Model):
     id=db.Column('user_id',db.Integer, primary_key=True)
+    rank=db.Column(db.Integer)
     name = db.Column(db.String(20))
     text=db.Column(db.String(1000))
-    file1=db.Column(db.String(20))
     personality_score=db.Column(db.Integer)
+    skills_score=db.Column(db.Integer)
+    experience_score=db.Column(db.Integer)
+    total_score=db.Column(db.Integer)
 
-    def __init__(self, name, text, file1, personality_score):
+    def __init__(self,rank, name, text, personality_score, skills_score, experience_score, total_score):
+        self.rank=rank
         self.name=name
         self.text=text
-        self.file1=file1
         self.personality_score= personality_score
+        self.experience_score=experience_score
+        self.skills_score=skills_score
+        self.total_score=total_score
+
+
         
 # load the model from disk
 nlp = spacy.load("en_core_web_sm")
@@ -116,14 +133,56 @@ def predict():
             my_prediction=personality_score
             f=request.files['upload']
             f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
-            data=database(request.form['name'],request.form['message'],f, personality_score)
+            resume_data = ResumeParser(f.filename).get_extracted_data()
+            print(resume_data)
+            skills=(resume_data.get("skills"))
+            experience=resume_data.get("total_experience")
+            
+            #Calculating SKills Score out of 40
+            skills_score=((len((text & set(skills))))/len(text))*40
+            
+            #Calculating Experience Score out of 40
+            if(experience>req_experience[1]):
+                experience_score=40
+            elif (experience<req_experience[0]):
+                experience_score=0
+            else:
+                experience_score=(experience/req_experience[1])*40
+            
+            candidate_total=experience_score+skills_score+(personality_score*0.2)
+            experience_score=experience_score*2.5
+            skills_score=skills_score*2.5
+            candidates = database.query.order_by(database.total_score._desc()).all() #fetch them all in one query
+            for (rank, database) in enumerate(candidates):
+                database.rank = rank + 1 #plus 1 cause enumerate starts from zero
+            data=database(rank,request.form['name'],request.form['message'], personality_score,skills_score, experience_score,candidate_total)
             db.session.add(data)
             db.session.commit()
             print('Record was successfully submitted')
+            
         else:
             my_prediction=3
         
-    return render_template('home.html',prediction = my_prediction)
+    return render_template('home.html',predicton=my_prediction)
+
+
+
+    #return render_template('resume.html')
+
+# @app.route('/resumesubmit')
+# def resumesubmit():
+#     return render_template('resume.html')
+
+# @app.route('/resume', methods=['POST'])
+# def resume():
+#     if request.method == 'POST':
+#         message = request.form['message']
+#         print(type(message))
+#         resume_data = ResumeParser(message).get_extracted_data()
+#         print (resume_data)
+#     return render_template('home.html')
+
+
 
 @app.route('/admin')
 def admin():
@@ -131,5 +190,6 @@ def admin():
 
 if __name__ == '__main__':
     db.create_all()
+    db.session.commit()
     app.run(debug=True)
 
